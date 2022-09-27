cfb20788984f5827f9a692c82cb01d2ec1b93503
Alis Gluhova <alis.gluhova03@gmail.com>
2022-09-27 00:32:29 -0700

    Create cinema_tickets.py

---
+++ cinema_tickets.py
@@ -0,0 +1,23 @@
+day=input()
+price_ticket=0
+if day=='Monday':
+    price_ticket=12
+    print(price_ticket)
+elif day=='Tuesday':
+    price_ticket=12
+    print(price_ticket)
+elif day=='Wednesday':
+    price_ticket=14
+    print(price_ticket)
+elif day=='Thursday':
+    price_ticket=14
+    print(price_ticket)
+elif day=='Friday':
+    price_ticket=12
+    print(price_ticket)
+elif day=='Saturday':
+    price_ticket=16
+    print(price_ticket)
+else:
+    price_ticket=16
+    print(price_ticket)
